36327fa667d72eba8ed04c9800cc023230f8a959
fathimaniba112 <fathimaniba9544@gmail.com>
2026-03-31 16:04:32 +0530

    save

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,7 +10,9 @@ const handleLogin = async (e) => {
   e.preventDefault();
 
   try {
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    // const res = await fetch("http://localhost:5000/api/auth/login", {
+        const res = await fetch("http://13.235.51.239/api/auth/login", {
+
       method: "POST",
       headers: {
         "Content-Type": "application/json",
