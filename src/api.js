54d8c2fb0284a094b46842d3053688942d386607
fathimaniba112 <fathimaniba9544@gmail.com>
2026-04-01 16:19:44 +0530

    save

--- src/api.js
+++ src/api.js
@@ -1,7 +1,15 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "/api"
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api"
+  baseURL: "http://13.201.63.42:5001/api"
 });
 
 export default API;
