a17771179b0b307946f43f96d41e14870e0024e4
fathimaniba112 <fathimaniba9544@gmail.com>
2026-04-02 10:07:11 +0530

    save

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,12 +1,21 @@
 export default async function handler(req, res) {
-  const response = await fetch("http://13.201.63.42:5001/api/auth/login", {
-    method: req.method,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(req.body),
-  });
+  const { path } = req.query;
+
+  const endpoint = path.join("/"); 
+  // example: auth/login
+
+  const response = await fetch(
+    `http://13.201.63.42:5001/api/${endpoint}`,
+    {
+      method: req.method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: req.method !== "GET" ? JSON.stringify(req.body) : undefined,
+    }
+  );
 
   const data = await response.json();
+
   res.status(response.status).json(data);
 }
