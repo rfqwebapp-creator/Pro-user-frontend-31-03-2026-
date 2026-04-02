7b0442249f57002728db36e9b0413235f8540e9e
fathimaniba112 <fathimaniba9544@gmail.com>
2026-04-02 12:44:11 +0530

    save

--- src/api.js
+++ src/api.js
@@ -8,7 +8,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://13.201.63.42:5001/api"
+  // baseURL: "http://13.201.63.42:5001/api"
+  baseURL: "http://13.232.250.5/api"
 });
 
 export default API;
